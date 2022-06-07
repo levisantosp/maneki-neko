f7a91f1732b8c4cbe45256f46b9813e5c8bd4589
Levi <levi.pires2005@gmail.com>
2022-06-07 16:08:26 -0300

    Update

---
+++ discord/src/commands/economy/granex.js
@@ -0,0 +1,22 @@
+const {Command} = require("../../structures");
+const {User} = require("../../../../database");
+
+module.exports = class extends Command {
+    constructor() {
+        super({
+            name: "granex",
+            aliases: ["money", "bal", "balance", "atm"],
+            description: "View your granex",
+            category: "economy"
+        });
+    }
+    async run(message) {
+        const u = await this.getUser(message.args[0] ?? message.author.mention);
+        const user = await User.findById(u.id);
+        if(!user) return message.reply("userIsNotInDatabase");
+        message.reply(user.id === message.author.id ? "yourGranex" : "hisgranex", {
+            granex: user.granex.toLocaleString(),
+            user: u.mention
+        });
+    }
+}
