3254d077619a2efe068777ec8a81e8957c8f56b1
Levi <levi.pires2005@gmail.com>
2022-06-07 16:24:15 -0300

    create a new function

--- discord/src/listeners/client/ready.js
+++ discord/src/listeners/client/ready.js
@@ -1,5 +1,5 @@
 const {Listener, Logger} = require("../../structures");
-const {User} = require("../../../../database");
+const {User, Bank} = require("../../../../database");
 
 module.exports = class extends Listener {
     constructor() {
@@ -8,7 +8,7 @@ module.exports = class extends Listener {
     async on() {
         Logger.send(`Logged as ${this.client.user.username}#${this.client.user.discriminator}`);
         this.client.editStatus("online", {
-            name: "I\"ll come back",
+            name: "I'll come back",
             type: 0
         });
         const reviveUser = async() => {
@@ -17,11 +17,37 @@ module.exports = class extends Listener {
                 user.energy = 500;
                 user.deadAt = null;
                 user.save();
-                Logger.warn(`(${user.id}) foi revivido.`);
+                Logger.warn(`(${user.id}) has been revived.`);
+            }
+        }
+        const addGranexPerFarm = async() => {
+            const users = await User.find({farmTime: {$lte: Date.now()}});
+            const bank = await Bank.findById("bank");
+            for(const user of users) {
+                if(user.farms.length >= 1) {
+                    var granexGain = 100;
+                    var n = 0;
+                    var n2 = 0;
+                    var n3 = 0;
+                    user.farms.forEach(farm => {
+                        if(farm.plants > 0) n += farm.plants * 5;
+                        if(farm.cows > 0) n2 += farm.cows * 5;
+                        if(farm.chickens > 0) n3 += farm.chickens * 5;
+                    });
+                    granexGain += user.farms.length * 10 + n + n2 + n3;
+                    if(bank.granex < granexGain) throw new Error("The bank doesn't have granex.");
+                    user.granex += granexGain;
+                    user.farmTime = Date.now() + 3.6e+6;
+                    bank.granex -= granexGain;
+                    user.save();
+                    bank.save();
+                    Logger.warn(`(${user.id}) received ${granexGain.toLocaleString()} granex per farm.`);
+                }
             }
         }
         setInterval(() => {
             reviveUser();
+            addGranexPerFarm().catch(err => new Logger(this.client).error(err));
         }, 10000);
     }
 }
