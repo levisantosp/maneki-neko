e275a6942bfc2ef1255f96f181cb42f901ef9a85
Levi <levi.pires2005@gmail.com>
2022-06-07 16:06:18 -0300

    Replace `asuras` => `granex`

--- database/schemas/bank.js
+++ database/schemas/bank.js
@@ -2,7 +2,7 @@ const {Schema, model} = require("mongoose");
 
 const bank = new Schema({
     _id: String,
-    asuras: {type: Number, default: 1000000}
+    granex: {type: Number, default: 1000000}
 });
 
 module.exports = model("bank", bank);
