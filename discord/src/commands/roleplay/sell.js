d11264c3038f6dc9346201178951429cc7182f8f
Levi <levi.pires2005@gmail.com>
2022-06-07 21:23:31 -0300

    Update

--- discord/src/commands/roleplay/sell.js
+++ discord/src/commands/roleplay/sell.js
@@ -639,11 +639,46 @@ module.exports = class SellCommand extends Command {
                 const execute = items[item];
                 execute();
             },
-            farm: function(member, toUser, value, item, client, locale) {
+            farm: async function(member, toUser, value, item, client, locale) {
+                const hasFarm = user?.farms?.filter(farm => farm.id === message.guild.id)[0];
+                if(!hasFarm) return message.reply("noFarm");
 
-            },
-            plant: function(member, toUser, value, item, client, locale) {
-
+                const button = new Button();
+                button.setStyle("GREEN");
+                button.setLabel("Accept");
+                button.setCustomID("confirm");
+                var msg = await message.replyC("sellConfirm", {
+                    components: [{
+                        type: 1,
+                        components: [button]
+                    }],
+                    price: Number(value),
+                    author: message.author.mention,
+                    user: member.mention
+                });
+                client.on("interactionCreate", async interaction => {
+                    if(interaction instanceof ComponentInteraction) {
+                        if(interaction.data.custom_id != "confirm") return;
+                        if(interaction.channel.id != message.channel.id) return;
+                        if(interaction.message.id != msg.id) return;
+                        if(interaction.member.id != toUser.id) return;
+                        await interaction.deferUpdate();
+                        if(toUser?.farm?.in?.includes(message.guild.id)) return interaction.createMessage({
+                            content: locale.get("alreadyBought"),
+                            flags: 64
+                        });
+                        var index = user.farms.indexOf(hasFarm);
+                        user.farms.splice(index, 1);
+                        toUser.farms.push(hasFarm);
+                        user.granex += Number.parseInt(value - (value * 0.1));
+                        toUser.granex -= Number.parseInt(value - (value * 0.1));
+                        bank.granex += Number.parseInt(value * 0.1);
+                        user.save();
+                        toUser.save();
+                        bank.save();
+                        message.reply("sellSucess");
+                    }
+                });
             }
         }
         const member = this.getMember(message.args[0]);
