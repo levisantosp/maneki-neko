48900b6a6741a7f72057cd6eb7b71dbfa698e486
Levi <levi.pires2005@gmail.com>
2022-06-13 18:41:43 -0300

    Create `rep` command

---
+++ discord/src/commands/social/rep.js
@@ -0,0 +1,28 @@
+const {Command} = require("../../structures");
+const {User} = require("../../../../database");
+
+module.exports = class RepCommand extends Command {
+    constructor() {
+        super({
+            name: "rep",
+            aliases: ["reputation"],
+            description: "Give a reputation for somebody",
+            category: "social"
+        });
+    }
+    async run(message) {
+        const member = await this.getUser(message.args[0]);
+        if(!member || member.id == message.author.id) return message.reply("invalidUser");
+        const user = await User.findById(message.member.id);
+        const toUser = await User.findById(member.id);
+        if(!toUser) return message.reply("userIsNotInDatabase");
+        if(user.repTime > Date.now()) {
+            return message.reply("repAlreadyGiven", {remaining: user.repTime});
+        }
+        toUser.reps += 1;
+        user.repTime = Date.now() + 3.6e+6;
+        toUser.save();
+        user.save();
+        message.reply("repGiven", {user: member.mention});
+    }
+}
