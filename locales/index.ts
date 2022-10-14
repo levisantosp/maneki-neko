a38875f1ba9d812ec914a37d6cc92dd2cc227650
Levi <levi.pires2005@gmail.com>
2022-10-14 19:27:21 -0300

    Removed the console.log

--- locales/index.ts
+++ locales/index.ts
@@ -3,9 +3,6 @@ import fs from 'fs'
 export const get = (lang: string, content: string, args?: any) => {
 	var locale = JSON.parse(fs.readFileSync(`locales/${lang}/locales.json`, 'utf-8'))
 
-	console.log(Object.keys(args))
-	console.log(args)
-
 	for (const file of content.split('.')) {
 		locale = locale[file]
 		if (!locale) return content
