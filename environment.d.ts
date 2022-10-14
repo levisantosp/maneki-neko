f436def9ddd63079681a1f3ff16ba050bf68ca23
Levi <levi.pires2005@gmail.com>
2022-10-14 19:23:08 -0300

    .

--- environment.d.ts
+++ environment.d.ts
@@ -1,16 +1,16 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            TOKEN: string
-            PREFIX: string
-            OWNER_ID: number
-            MONGO_URI: string
-            BOT_INVITE: string
-            SUPPORT_SERVER: string
-            IMGUR_CLIENT_ID: string
-            IMGUR_CLIENT_SECRET: string
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      TOKEN: string
+      PREFIX: string
+      OWNER_ID: number
+      MONGO_URI: string
+      BOT_INVITE: string
+      SUPPORT_SERVER: string
+      IMGUR_CLIENT_ID: string
+      IMGUR_CLIENT_SECRET: string
     }
+  }
 }
 
-export {}
+export { }
